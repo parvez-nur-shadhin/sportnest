4cdffb931cc1ca75065ee4d34a8bade9d5f655ff
parvez-nur-shadhin <parveznur00@gmail.com>
2026-05-18 01:00:37 +0600

    Login Page Completed, Register Page Completed, Better Auth authentication with Mongodb completed

---
+++ src/app/(auth)/login/page.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+const LoginPage = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data) => {
+    const { data:res, error } = await authClient.signIn.email({
+      email: data.email, // required
+      password: data.password, // required
+      rememberMe: true,
+      callbackURL: "/",
+    });
+    console.log(res, error);
+  };
+
+  return (
+    <div className="container mx-auto my-15">
+      <h1 className="text-center text-6xl font-bold text-white">
+        Log in to continue
+      </h1>
+      <div className="mx-auto my-10">
+        <form className="mx-auto" onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset bg-none border-base-300 rounded-box w-xs md:w-lg border p-4 mx-auto space-y-2">
+            <label className="label text-white text-md">Email</label>
+            <input
+              type="email"
+              className="input w-full"
+              placeholder="Email"
+              {...register("email")}
+            />
+
+            <label className="label text-white text-md">Password</label>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Password"
+              {...register("password")}
+            />
+            <h1 className="text-white">
+              Didn't Registered Yet?{" "}
+              <Link
+                className=" underline font-semibold cursor-pointer text-[#aaf40c]"
+                href={"/register"}
+              >
+                Register
+              </Link>
+            </h1>
+
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+const RegisterPage = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data) => {
+    const { data:res, error } = await authClient.signUp.email({
+      name: data.name, // required
+      email: data.email, // required
+      password: data.password, // required
+      image: data.image,
+      callbackURL: "/login",
+    });
+    console.log(res, error);
+  };
+
+  return (
+    <div className="container mx-auto my-15">
+      <h1 className="text-center text-6xl font-bold text-white">
+        Log in to continue
+      </h1>
+      <div className="mx-auto my-10">
+        <form className="mx-auto" onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset bg-none border-base-300 rounded-box w-xs md:w-lg border p-4 mx-auto space-y-2">
+            <label className="label text-white text-md">Name</label>
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="Enter Your Name"
+              {...register("name")}
+            />
+            <label className="label text-white text-md">Email</label>
+            <input
+              type="email"
+              className="input w-full"
+              placeholder="Email"
+              {...register("email")}
+            />
+            <label className="label text-white text-md">Image</label>
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="Enter Your Image URL"
+              {...register("image")}
+            />
+
+            <label className="label text-white text-md">Password</label>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Password"
+              {...register("password")}
+            />
+            <h1 className="text-white">
+              Already Have an account?
+              <Link
+                className=" underline font-semibold cursor-pointer text-[#aaf40c]"
+                href={"/login"}
+              >
+                Login
+              </Link>
+            </h1>
+
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
