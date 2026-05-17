d1e36c79eb006799b81f2b5d4f9b4f5ef24c6165
parvez-nur-shadhin <parveznur00@gmail.com>
2026-05-17 21:05:58 +0600

    Initial Navbar done and fond added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import { MdSportsCricket } from "react-icons/md";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li className="font-medium text-md">
+        <Link href={"/"}>
+          <h1>Home</h1>
+        </Link>
+      </li>
+      <li className="font-medium text-md">
+        <Link href={"/all-facilities"}>
+          <h1>All Facilities</h1>
+        </Link>
+      </li>
+      <li className="font-medium text-md">
+        <Link href={"/my-bookings"}>
+          <h1>My Bookings</h1>
+        </Link>
+      </li>
+      <li className="font-medium text-md">
+        <Link href={"/add-facilities"}>
+          <h1>Add Facility</h1>
+        </Link>
+      </li>
+      <li className="font-medium text-md">
+        <Link href={"/my-facilities"}>
+          <h1>My Facilities</h1>
+        </Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div>
+      <div className="navbar bg-none p-4 container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <Link href={"/"}>
+            <h1 className="font-(family-name:--font-smooch-sans) text-5xl font-bold flex items-center gap-2">
+              <MdSportsCricket className="text-[#aaf40c]" /> SportNest
+            </h1>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end">
+          <Link href={"/login"}>
+            <button className="btn bg-[#aaf40c] text-black">Login</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bitter, Geist, Geist_Mono, Smooch_Sans } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/Components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,18 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const smoochSans = Smooch_Sans({
+  variable: "--font-smooch-sans",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+const bitter = Bitter({
+  variable: "--font-bitter",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
 export const metadata = {
   title: "SportNest",
   description: "Created by Parvez Nur Shadhin",
@@ -20,9 +33,11 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      data-theme="light"
+      className={`${geistSans.variable} ${geistMono.variable} ${smoochSans.variable} ${bitter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col font-(family-name:--font-bitter)">
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
