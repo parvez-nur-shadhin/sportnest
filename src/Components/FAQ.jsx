fefb781861d4d47a91c525132f6b80dafa1295cb
parvez-nur-shadhin <parveznur00@gmail.com>
2026-05-21 02:38:29 +0600

    FAQ section added

---
+++ src/Components/FAQ.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const FAQ = () => {
+  return (
+    <div className="container mx-auto space-y-4 p-4 my-10">
+        <h1 className="text-white text-center font-bold text-3xl mb-5g">Frequently Asked Questions</h1>
+      <div className="space-y-4">
+        <div className=" collapse collapse-plus bg-[#aaf40c] border border-base-300">
+          <input type="radio" name="my-accordion-3" defaultChecked />
+          <div className="collapse-title font-semibold">
+            What types of facilities can I book?
+          </div>
+          <div className="collapse-content text-sm">
+            You can book different types of sports and event facilities,
+            including football fields, badminton courts, basketball courts, and
+            indoor event spaces.
+          </div>
+        </div>
+        <div className="collapse collapse-plus bg-[#aaf40c] border border-base-300">
+          <input type="radio" name="my-accordion-3" />
+          <div className="collapse-title font-semibold">
+            Can I cancel or reschedule my booking?
+          </div>
+          <div className="collapse-content text-sm">
+            Yes, you can cancel or reschedule your booking depending on
+            availability and the cancellation policy of the facility.
+          </div>
+        </div>
+        <div className="collapse collapse-plus bg-[#aaf40c] border border-base-300">
+          <input type="radio" name="my-accordion-3" />
+          <div className="collapse-title font-semibold">
+            How do I confirm my booking payment?
+          </div>
+          <div className="collapse-content text-sm">
+            After completing the payment, you will receive a booking
+            confirmation message and email with all your reservation details.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FAQ;
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Banner from "@/Components/Banner";
+import FAQ from "@/Components/FAQ";
 import FeaturedFacilities from "@/Components/FeaturedFacilities";
 import WhyChooseUs from "@/Components/WhyChooseUs";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Banner />
       <FeaturedFacilities />
       <WhyChooseUs />
+      <FAQ />
     </div>
   );
 }
